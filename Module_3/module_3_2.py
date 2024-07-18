e19e6947d624956d05abd4aee624f37ca76f9138
R-S4F <151863124+R-S4F@users.noreply.github.com>
2024-07-19 00:08:32 +0500

    Commit

--- Module_3/module_3_2.py
+++ Module_3/module_3_2.py
@@ -1,23 +1,24 @@
 # Домашняя работа по уроку "Способы вызова функции"
 
-def send_email(message, recipient, *, sender='university.help@gmail.com'):
-    def checker(email):
-        domens_for_check = ('.com', '.ru', '.net')
-        check = False
-        if email == 'university.help@gmail.com':
-            check = True
-        else:
-            for i in domens_for_check:
-                if email.replace(i, '') == email[0:len(email) - len(i)]:
-                    for k in email[-len(i) - 1::-1]:
-                        if k == '@':
-                            check = True
-                            break
-                if check:
-                    break
-        return check
+def checker(email):
+    domains_for_check = ('.com', '.ru', '.net')
+    check = False
+    if email == 'university.help@gmail.com':
+        check = True
+    else:
+        for i in domains_for_check:
+            if email.replace(i, '') == email[0:len(email) - len(i)]:
+                for k in email[-len(i) - 1::-1]:
+                    if k == '@':
+                        check = True
+                        break
+            if check:
+                break
+    return check
 
-    # Endline of checker
+# Endline of checker
+
+def send_email(message, recipient, *, sender='university.help@gmail.com'):
 
     if not (checker(sender) and checker(recipient)):
         print(f'Невозможно отправить письмо с адреса <{sender}> на адрес <{recipient}>')
@@ -29,7 +30,6 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
                 print(f"Письмо успешно отправлено с адреса <{sender}> на адрес <{recipient}>.")
             else:
                 print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <{sender}> на адрес <{recipient}>.")
-
 
 # Endline of send_email
 
