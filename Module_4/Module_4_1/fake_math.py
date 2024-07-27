e3d3fc292caf7c0c96de64bed03be880c5ff6127
r_s4f <r.s4f-alpha@ya.ru>
2024-07-28 02:51:57 +0500

    ggg

---
+++ Module_4/Module_4_1/fake_math.py
@@ -0,0 +1,8 @@
+from math import inf
+
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return inf
---
+++ Module_4/Module_4_1/true_math.py
@@ -0,0 +1,7 @@
+
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return 'ОШИБКА! Деление на ноль невозможно.'
