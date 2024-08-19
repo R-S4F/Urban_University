bcbec23a85d71ce7e92974fea0c046b78c3012fe
r_s4f <r.s4f-alpha@ya.ru>
2024-08-19 20:45:56 +0500

    Module_6_1.py

---
+++ Module_6/Module_6_1/Module_6_1.py
@@ -0,0 +1,59 @@
+# Домашнее задание по теме "Зачем нужно наследование"
+
+class Animal:
+    def __init__(self, name, alive=True, fed=False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+    def eat(self, food):
+        if food.edible is True:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+        pass
+
+
+class Plant:
+    def __init__(self, name, edible=False):
+        self.name = name
+        self.edible = edible
+
+
+class Mammal(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Predator(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Flower(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+class Fruit(Plant):
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
