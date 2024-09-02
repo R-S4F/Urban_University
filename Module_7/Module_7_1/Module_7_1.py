294514d4199d9aaa0ca7e32305a8ef2f79ae25a4
r_s4f <r.s4f-alpha@ya.ru>
2024-09-03 00:35:15 +0500

    Module_7_1.py

---
+++ Module_7/Module_7_1/Module_7_1.py
@@ -0,0 +1,45 @@
+# Домашнее задание по теме "Режимы открытия файлов"
+
+from pprint import pprint
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.category = str(category)
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        __P1 = open(self.__file_name, 'r')
+        _str = str(__P1.read())
+        __P1.close()
+        return _str
+
+    def add(self, *products):
+        __P1 = open(self.__file_name, 'r+')
+        for i in products:
+            if str(i) not in self.get_products():
+                __P1.write(f'{i}\n')
+            else:
+                pprint(f'Продукт {i} уже есть в магазине')
+        __P1.close()
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
