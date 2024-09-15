5ab0664ee21c64b2ae3c7ecb624f7638783dcc39
r_s4f <r.s4f-alpha@ya.ru>
2024-09-16 04:10:17 +0500

    Module_9_5

---
+++ Module_9/m95/main.py
@@ -0,0 +1,54 @@
+# Домашнее задание по теме "Итераторы"
+class StepValueError(ValueError):
+    pass
+
+
+class Iterator:
+
+    def __init__(self, start, stop, step=1):
+        if step == 0:
+            raise StepValueError('Шаг не может быть равен 0')
+        if not isinstance(start, int) or not isinstance(stop, int) or not isinstance(step, int):
+            raise TypeError('Получено не целое число')
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.pointer = start
+
+    def __iter__(self):
+        self.pointer = self.start
+        return self
+
+    def __next__(self):
+        if self.step < 0 and self.pointer < self.stop:
+            raise StopIteration()
+        elif self.step > 0 and self.pointer > self.stop:
+            raise StopIteration()
+        self.pointer += self.step
+        return self.pointer - self.step
+
+
+try:
+    iter1 = Iterator(100, 200, 0)
+    for i in iter1:
+        print(i, end=' ')
+except StepValueError:
+    print('Шаг указан неверно')
+
+iter2 = Iterator(-5, 1)
+iter3 = Iterator(6, 15, 2)
+iter4 = Iterator(5, 1, -1)
+iter5 = Iterator(10, 1)
+
+for i in iter2:
+    print(i, end=' ')
+print()
+for i in iter3:
+    print(i, end=' ')
+print()
+for i in iter4:
+    print(i, end=' ')
+print()
+for i in iter5:
+    print(i, end=' ')
+print()
